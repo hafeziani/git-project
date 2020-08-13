2744b88113be78fe41e7373f0e0cf8d0ab0fcb39
Hafez <Hafezjokar@gmail.com>
2020-08-13 16:31:41 +0200

    fix sql injection bug for login.php

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+$db = mysqli_connect('localhost', 'root', '');
+mysqli_select_db($db, 'db1');
+mysqli_query($db, "SET NAMES 'UTF8'");
+$username = htmlentities(addslashes($_POST('username')), ENT_QUOTES, 'UTF-8');
+$password = htmlentities(addslashes($_POST('password')), ENT_QUOTES, 'UTF-8');
+$q = "SELECT * FROM useres WHERE username = '$username'
+        AND password = '$password'";
+
+$r = mysqli_query($db, $q);
+$user = mysqli_fetch_assoc($r);
+if (isset($user['id']))
+{
+    print "Login OK";
+}
+else
+{
+    print "Login Fail";
+}
+
+?>
